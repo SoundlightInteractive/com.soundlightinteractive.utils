76d664bd6fd506211ebbec9a67dd7044744b1d3f
Onur ÖZEL <sumotoma_onur@hotmail.com>
2024-04-28 11:42:21 +0300

    Add files via upload

---
+++ Runtime/SIDebug.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace SoundlightInteractive.Utils
+{
+    public static class SIDebug
+    {
+        [System.Diagnostics.Conditional("ENABLE_LOG")]
+        public static void Log(string message)
+        {
+            Debug.Log(message);
+        }
+        
+        [System.Diagnostics.Conditional("ENABLE_LOG")]
+        public static void LogWarning(string message)
+        {
+            Debug.LogWarning(message);
+        }
+        
+        [System.Diagnostics.Conditional("ENABLE_LOG")]
+        public static void LogError(string message)
+        {
+            Debug.LogError(message);
+        }
+    }
+}
